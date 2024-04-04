b14d657fbbadb610cf5e7dab541c176e4d240786
changon <tyu4778@gmail.com>
2024-04-04 22:57:21 +0900

    fix: Root path 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Outlet, Routes, Route } from "react-router-dom";
-import './App.css';
-import Nav from './components/Nav';
+import "./App.css";
+import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import MainPage from './pages/MainPage/index';
-import SearchPage from './pages/SearchPage/index';
-import DetailPage from './pages/DetailPage/index';
+import MainPage from "./pages/MainPage/index";
+import SearchPage from "./pages/SearchPage/index";
+import DetailPage from "./pages/DetailPage/index";
 
 const Layout = () => {
   return (
@@ -16,21 +16,21 @@ const Layout = () => {
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Layout />} >
+        <Route path="/pop-film" element={<Layout />}>
           <Route index element={<MainPage />} />
           <Route path=":movieId" element={<DetailPage />} />
           <Route path="search" element={<SearchPage />} />
         </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
